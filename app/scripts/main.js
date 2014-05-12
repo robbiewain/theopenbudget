3747bfd97fa12f608c29d99a33c2fa61f8ba4a38
robbiewain <robbiewain@gmail.com>
2014-05-12 23:06:27 +1000

    update greens and reds thresholds

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -367,41 +367,41 @@ function updateColors(d) {
     } else {
         if (showChanges) {
             var pc = 0;
-            pc = (d.v2 - d.v1) / d.v2 * 100;
+            pc = (d.v2 - d.v1) / d.v2;
             if (pc < -0.64) {
-                c = reds[7];
+                c = reds[8];
             } else if (pc < -0.32) {
-                c = reds[6];
+                c = reds[7];
             } else if (pc < -0.16) {
-                c = reds[5];
+                c = reds[6];
             } else if (pc < -0.08) {
-                c = reds[4];
+                c = reds[5];
             } else if (pc < -0.04) {
-                c = reds[3];
+                c = reds[4];
             } else if (pc < -0.02) {
-                c = reds[2];
+                c = reds[3];
             } else if (pc < -0.01) {
-                c = reds[1];
+                c = reds[2];
             } else if (pc < 0) {
-                c = reds[0];
+                c = reds[1];
             } else if (pc === 0) {
                 c = "777";
             } else if (pc < 0.01) {
-                c = greens[0];
-            } else if (pc < 0.02) {
                 c = greens[1];
-            } else if (pc < 0.04) {
+            } else if (pc < 0.02) {
                 c = greens[2];
-            } else if (pc < 0.08) {
+            } else if (pc < 0.04) {
                 c = greens[3];
-            } else if (pc < 0.16) {
+            } else if (pc < 0.08) {
                 c = greens[4];
-            } else if (pc < 0.32) {
+            } else if (pc < 0.16) {
                 c = greens[5];
-            } else if (pc < 0.64) {
+            } else if (pc < 0.32) {
                 c = greens[6];
-            } else {
+            } else if (pc < 0.64) {
                 c = greens[7];
+            } else {
+                c = greens[8];
             }
         } else {
             c = color(d.i);
